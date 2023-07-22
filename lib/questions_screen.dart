f2010011424c271936f78ff576c7fe6edd0cfac5
Victor Allen <vamuigua@gmail.com>
2023-07-22 20:54:40 +0300

    (UI)update: added google fonts flutter package for changing text style fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 import "package:quiz_app/answer_button.dart";
 import "package:quiz_app/data/questions.dart";
 
@@ -34,7 +35,11 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(
