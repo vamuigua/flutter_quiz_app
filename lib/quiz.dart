65337c164e5ca68c2c0866eadd73db5f47b5379e
Victor Allen <vamuigua@gmail.com>
2023-08-09 10:01:16 +0300

    update(Quiz): created results-screen to show answers selected by the user

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
+
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
 
@@ -28,7 +30,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -41,6 +43,10 @@ class _QuizState extends State<Quiz> {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
       );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = const ResultsScreen();
     }
 
     return MaterialApp(
