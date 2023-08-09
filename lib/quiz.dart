f54194e3b959b87bcdb67b0259edc33f83b0b63c
Victor Allen <vamuigua@gmail.com>
2023-08-09 14:00:24 +0300

    update(Results): add quiz results summary data and formatted styling

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -29,7 +29,6 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        selectedAnswers = [];
         activeScreen = 'results-screen';
       });
     }
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
@@ -37,7 +38,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text("List of answers and questions..."),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
